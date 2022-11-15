be0a8f0382ea0ba29b60fb33f0ca77fcf2184ea3
Igor Alonge <igoroalonge@gmail.com>
2022-11-15 17:11:39 -0300

    fix: add header param check

--- src/infra/axios-http-client/axios-http-client.ts
+++ src/infra/axios-http-client/axios-http-client.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 export class AxiosHttpClient implements HttpPostClient<any, any> {
   async post (params: HttpPostParams<any>): Promise<HttpResponse<any>> {
-    const httpResponse = await axios.post(params.url, params.body, {
+    const httpResponse = await axios.post(params.url, params.body, params.headers && {
       headers: params.headers
     })
     
